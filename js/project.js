7fd08cdf4985c528437b831006717891c496df26
japi <japi@umich.edu>
2020-06-23 00:13:08 -0400

    first commit

---
+++ js/project.js
@@ -0,0 +1,57 @@
+
+const appearOptions = {
+    threshhold: 1,
+    rootMargin: "0px 0px -250px 0px"
+};
+
+const faders = document.querySelectorAll('.fade');
+const sliders = document.querySelectorAll('.slide');
+
+const appearOnScroll = new IntersectionObserver (
+    function(entries, appearOnScroll) {
+    entries.forEach(entry => {
+        if(!entry.isIntersecting) {
+            return;
+        } else {
+            entry.target.classList.add('appear');
+            appearOnScroll.unobserve(entry.target);
+        }
+    })
+}, appearOptions);
+
+faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+});
+
+const slideOnScroll = new IntersectionObserver (
+    function(entries, appearOnScroll) {
+    entries.forEach(entry => {
+        if(!entry.isIntersecting) {
+            return;
+        } else {
+            entry.target.classList.add('slide-in');
+            appearOnScroll.unobserve(entry.target);
+        }
+    })
+}, appearOptions);
+
+sliders.forEach(slider => {
+    slideOnScroll.observe(slider);
+});
+
+
+window.onload = () => {
+    document.getElementsByClassName('project-page-thumb')[0].classList.add('thumb-transition');
+    document.getElementsByClassName('project-page-description')[0].classList.add('description-grow');
+};
+
+//resize logo on scroll
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 75 || document.documentElement.scrollTop > 75) {
+    document.getElementsByClassName("logo")[0].classList.add('logo-shrink');
+  } else {
+    document.getElementsByClassName("logo")[0].classList.remove('logo-shrink');
+  }
+}
---
+++ js/script.js
@@ -0,0 +1,151 @@
+// Credits to Matt Boldt for typed.js 
+// https://github.com/mattboldt/typed.js
+
+var project_thumbs = document.getElementsByClassName('project-thumb');
+var project_titles = document.getElementsByClassName('project-title');
+var curr_project = 0;
+var gal_left_btn = document.getElementById('gal-left');
+var gal_right_btn = document.getElementById('gal-right');
+
+gal_left_btn.addEventListener("click", galLeft);
+gal_right_btn.addEventListener("click", galRight);
+
+// project tags
+var typed_arr = ['speculative design | installation', 
+                 'R&#38;D | hardware prototype', 
+                 'user research | hardware prototype',
+                 'architecture',
+                 'hardware prototype | robotics',
+                 'R&#38;D | hardware prototype'];
+
+
+var typed = new Typed('#project-tags', {
+        strings: [typed_arr[curr_project]],
+        typeSpeed: 20,
+        backSpeed: 0,
+        showCursor: false,
+        loop: false,
+});
+
+// show first project 
+project_thumbs[curr_project].classList.add('project-fade');
+project_titles[curr_project].classList.add('title-fade');
+
+
+
+
+//key pressed state
+var pressed = false;
+
+document.onkeydown = checkKey;
+
+function checkKey(e) {
+
+    e = e || window.event;
+
+    if (e.keyCode == '37') {
+       // left arrow
+        if (!pressed) {
+            galLeft();
+            pressed = true;
+            setTimeout(() => {
+                pressed = false;
+            }, 800);
+        }
+    }
+    else if (e.keyCode == '39') {
+       // right arrow
+        if (!pressed) {
+            galRight();
+            pressed = true;
+            setTimeout(() => {
+                pressed = false;
+            }, 800);
+        }
+    }
+
+}
+
+// move to left project
+function galLeft () {
+    
+    disableButton();
+    
+    // hide previous project
+    setTimeout(() => {
+        project_thumbs[curr_project].classList.remove('project-fade');
+        project_titles[curr_project].classList.remove('title-fade');
+    }, 0);
+    
+        setTimeout(() => {
+            if (curr_project == 0) {
+            curr_project = project_thumbs.length - 1;
+            } else {
+                curr_project -= 1;
+            }
+
+            // destroys existing type obj and updates
+            typed.destroy();
+            typed = new Typed('#project-tags', {
+                strings: [typed_arr[curr_project]],
+                typeSpeed: 30,
+                backSpeed: 0,
+                showCursor: false,
+                loop: false,
+            });
+
+            project_thumbs[curr_project].classList.add('project-fade');
+            project_titles[curr_project].classList.add('title-fade');
+
+        }, 200);    
+    
+    
+    
+}
+
+
+    
+// move to left project
+function galRight () {
+    
+    disableButton();
+    
+    // hide previous project
+    setTimeout(() => {
+        project_thumbs[curr_project].classList.remove('project-fade');
+        project_titles[curr_project].classList.remove('title-fade');
+    }, 0);
+    
+    setTimeout(() => {
+        if (curr_project == project_thumbs.length - 1) {
+        curr_project = 0;
+        } else {
+            curr_project += 1;
+        }
+        
+        // destroys existing type obj and updates
+        typed.destroy();
+        typed = new Typed('#project-tags', {
+            strings: [typed_arr[curr_project]],
+            typeSpeed: 30,
+            backSpeed: 0,
+            showCursor: false,
+            loop: false,
+        });
+        
+        project_thumbs[curr_project].classList.add('project-fade');
+        project_titles[curr_project].classList.add('title-fade');
+    }, 200);
+}
+
+
+//disables buttons for 1s and reenables
+function disableButton () {
+    gal_left_btn.removeEventListener("click", galLeft);
+    gal_right_btn.removeEventListener("click", galRight);
+    
+    setTimeout(() => {
+        gal_left_btn.addEventListener("click", galLeft);
+        gal_right_btn.addEventListener("click", galRight);
+    }, 800);
+}
