915a7c5d94b9f93faa7ecd2fb514d01ce282bec2
japi <japi@umich.edu>
2020-11-19 18:24:32 -0800

    menu bug

--- js/menu.js
+++ js/menu.js
@@ -1,31 +1,36 @@
 //check if menu opened 
 
-var menu_hamburger = document.getElementById("menu-hamburger");
+var menu_hamburger;
+
+window.addEventListener("load", ()=> {
+    
+    menu_hamburger = document.getElementById("menu-hamburger");
+    menu_hamburger.addEventListener("click", toggleMenu);
+})
+
 var menu_full = document.getElementsByClassName("full-menu");
 
 
 function toggleMenu() {
     
-    if (menu_hamburger.checked == true) {
-        menu_full[0].style.display = "block";
-        setTimeout(()=>{
-            menu_full[0].classList.add("menu-open");
-        },20);
-        document.body.style.overflowY = "hidden";
-    }
-    else {
-        
-        menu_full[0].classList.remove("menu-open");
-        menu_full[0].style.display = "none";
-        document.body.style.overflowY = "auto";
-    }
+    //if (menu_hamburger) {
+        if (menu_hamburger.checked == true) {
+            menu_full[0].style.display = "block";
+            setTimeout(()=>{
+                menu_full[0].classList.add("menu-open");
+            },20);
+            document.body.style.overflowY = "hidden";
+        }
+        else {
+
+            menu_full[0].classList.remove("menu-open");
+            menu_full[0].style.display = "none";
+            document.body.style.overflowY = "auto";
+        }
+    //}
+    
 }
 
-
-//test if menu is loaded
-if (menu_hamburger) {
-    menu_hamburger.addEventListener("click", toggleMenu);
-}
 
 
     
